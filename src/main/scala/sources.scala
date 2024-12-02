05d01febbbc0f091b43ecd674571fa62f5c967df
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-02 11:24:52 +0100

    day1

---
+++ src/main/scala/sources.scala
@@ -0,0 +1,15 @@
+import scala.util.{Failure, Success, Try}
+
+@main
+def getSource(fileName: String): List[String] = {
+   val cwd = System.getProperty("user.dir")
+   val src = Try(
+     scala.io.Source
+        .fromFile(s"$cwd/sources/$fileName"))
+   src match
+      case Success(file) =>
+         val input = file.getLines().toList
+         file.close()
+         input
+      case Failure(_) => Nil
+}
