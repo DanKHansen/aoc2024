89b0918af8df9ed1af279c650db2b2f521c55e88
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-03 10:57:11 +0100

    day2 part two

--- src/main/scala/day2.scala
+++ src/main/scala/day2.scala
@@ -1,30 +1,21 @@
-import scala.annotation.tailrec
-
 @main
 def day2(): Unit = {
-   val reports = getSource("2_test.txt") map (s => s.split(" ").toList.map(_.toInt))
+   val reports = getSource("2.txt").map(str => str.split(" ").toList.map(_.toInt))
 
-   def listOfDiffs(l: List[Int]) = l.sliding(2).map { case Seq(a, b) => b - a }.toList
+   def listOfDiffs(report: List[Int]) = report.sliding(2).map { case Seq(lvl1, lvl2) => lvl2 - lvl1 }.toList
 
-   def isReportSafe(l: List[Int]): Boolean = {
-      val ll = listOfDiffs(l)
-      (ll.forall(_ > 0) || ll.forall(_ < 0))
-      && ll.forall(diff => math.abs(diff) <= 3)
-      && !ll.contains(0)
+   def isReportSafe(report: List[Int]): Boolean = {
+      val diffs = listOfDiffs(report)
+      (diffs.forall(_ > 0) || diffs.forall(_ < 0))
+      && diffs.forall(diff => math.abs(diff) <= 3)
+      && !diffs.contains(0)
    }
 
-
-   @tailrec
-   def isReportStillSafe(l: List[Int]): Boolean =
-      if isReportSafe(l) then true else
-         isReportStillSafe(l.tail)
-         // try to remove one level at a time and re-check the remaining report
-         // if all possibilities have been exhausted return false
-
+   def isReportStillSafe(report: List[Int]): Boolean =
+      isReportSafe(report) || report.indices.exists(idx => isReportSafe(report.patch(idx, Nil, 1)))
 
    val first = reports.map(isReportSafe).count(_ == true)
-
-   val second = reports.map(r => isReportStillSafe(r)).count(_ == true)
+   val second = reports.map(isReportStillSafe).count(_ == true)
 
    println(first)
    println(second)
