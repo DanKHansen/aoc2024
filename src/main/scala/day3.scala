73bdb9865556985cd312f32db2226c0512f0cc44
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-03 11:53:12 +0100

    day3 part two

--- src/main/scala/day3.scala
+++ src/main/scala/day3.scala
@@ -3,6 +3,7 @@ def day3(): Unit = {
    val memory = getSource("3.txt").mkString
    val pattern = "mul\\((\\d+),(\\d+)\\)".r
    val removePattern = "don't\\(\\)(.*?)(do\\(\\)|$)"
+
    def mul(x: Int, y: Int) = x * y
 
    val first = pattern.findAllMatchIn(memory).foldLeft(0) { case (sum, pat) =>
