3b8cb14a4fdd6a84789cc058934d681872a031f4
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-04 15:35:39 +0100

    refactoring

--- src/main/scala/day4.scala
+++ src/main/scala/day4.scala
@@ -1,52 +1,57 @@
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 @main
 def day4(): Unit = {
    val grid = getSource("4.txt")
-   val lines = grid.indices
-   val positions = grid.head.indices
    val directions = List("n", "ne", "e", "se", "s", "sw", "w", "nw")
-   val directions2 = directions.filter(_.length == 2)
-
-   println(s"1: ${countXmas(directions, "xmas", grid)}")
-   println(s"2: ${countWords(directions2, "mas", grid)}")
 
    def countWords(dirs: List[String], word: String, grid: List[String]) =
+      val wordUpper = word.toUpperCase
       val rng = -word.length / 2 to word.length / 2
-      (for
-         l <- grid.indices
-         p <- grid.head.indices
-      yield find(grid, dirs, rng, l, p))
-         .map(l => l.count(_ == word.toUpperCase.toVector) == 2 && l.count(_ == word.toUpperCase.toVector) == 2)
-         .count(_ == true)
+      grid.indices
+         .flatMap(l =>
+            grid.head.indices
+               .map(p =>
+                  val found = find(grid, dirs, rng, l, p)
+                  (p, found))
+               .map { case (_, found) =>
+                  found.count(_ == wordUpper.toVector)
+               })
+         .count(_ == 2)
 
-   def countXmas(dirs: List[String], word: String, grid: List[String]): Int =
+   def countXmas(dirs: List[String], word: String, grid: List[String]): Int = {
+      val wordUpper = word.toUpperCase
       val rng = 0 until word.length
-      (for
-         l <- grid.indices
-         p <- grid.head.indices
-      yield find(grid, dirs, rng, l, p).map(v => v.mkString == word.toUpperCase)).flatten
-         .count(_ == true)
+      grid.indices.flatMap { l =>
+         grid.head.indices.flatMap { p =>
+            find(grid, dirs, rng, l, p).filter(_.mkString == wordUpper)
+         }
+      }.size
+   }
 
    def star(pos: Int, lin: Int, rng: Range): Map[String, Seq[(Int, Int)]] =
-      Map(
-        "n" -> (for i <- rng yield (pos, lin - i)),
-        "ne" -> (for i <- rng yield (pos + i, lin - i)),
-        "e" -> (for i <- rng yield (pos + i, lin)),
-        "se" -> (for i <- rng yield (pos + i, lin + i)),
-        "s" -> (for i <- rng yield (pos, lin + i)),
-        "sw" -> (for i <- rng yield (pos - i, lin + i)),
-        "w" -> (for i <- rng yield (pos - i, lin)),
-        "nw" -> (for i <- rng yield (pos - i, lin - i))
-      )
+      directions.map { dir =>
+         dir -> (for (i <- rng) yield dir match {
+            case "n"  => (pos, lin - i)
+            case "ne" => (pos + i, lin - i)
+            case "e"  => (pos + i, lin)
+            case "se" => (pos + i, lin + i)
+            case "s"  => (pos, lin + i)
+            case "sw" => (pos - i, lin + i)
+            case "w"  => (pos - i, lin)
+            case "nw" => (pos - i, lin - i)
+         })
+      }.toMap
 
-   def find(grid: List[String], dirs: List[String], rng: Range, lin: Int, pos: Int): List[IndexedSeq[Char]] =
+   def find(grid: List[String], dirs: List[String], rng: Range, lin: Int, pos: Int): List[Seq[Char]] =
       dirs.map { dir =>
          val v = star(pos, lin, rng).getOrElse(dir, Nil)
-         for i <- v.indices
-         yield Try(grid(v(i)._2)(v(i)._1)) match
-            case Failure(_)     => ' '
-            case Success(value) => value
+         v.map { case (x, y) =>
+            Try(grid(y)(x)).getOrElse(' ')
+         }
       }
+
+   println(s"1: ${countXmas(directions, "xmas", grid)}")
+   println(s"2: ${countWords(directions.filter(_.length == 2), "mas", grid)}")
 
 }
