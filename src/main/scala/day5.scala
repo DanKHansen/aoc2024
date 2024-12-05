9f207a8bb837ffccef061d5b902f4f6de8425416
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-05 12:33:32 +0100

    day5 1 solution

--- src/main/scala/day5.scala
+++ src/main/scala/day5.scala
@@ -1,8 +1,29 @@
+import scala.annotation.tailrec
+
 @main
 def day5(): Unit = {
-   val source = getSource("5_test.txt")
+   val source = getSource("5.txt")
+   val rules: Seq[(Int, Int)] =
+      source.takeWhile(_ != "").map { line =>
+         val Array(first, last) = line.split("\\|").map(_.toInt)
+         (first, last)
+      }
+   val updates: Seq[Vector[Int]] =
+      source.dropWhile(_ != "").tail.map(_.split(",").map(_.toInt).toVector)
 
-   println(s"1: ${}")
-   println(s"2: ${}")
+   def getMiddleNumber(v: Vector[Int]): Int = v(v.length / 2)
+
+   @tailrec
+   def checkFwd(v: Vector[Int], r: Seq[(Int, Int)]): Boolean =
+      if (v.isEmpty) true
+      else {
+         val head = v.head
+         val tail = v.tail
+         if (tail.forall(i => r.contains((head, i)))) checkFwd(tail, r)
+         else false
+      }
+
+   println(s"1: ${updates.map(u => if (checkFwd(u, rules)) getMiddleNumber(u) else 0).sum}")
+   // println(s"2: ${}")
 
 }
