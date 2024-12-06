8ae49b6c7257084bc90b3c5f6e7d2e71ad0d2155
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-06 10:52:16 +0100

    day5 2 solution

--- src/main/scala/day5.scala
+++ src/main/scala/day5.scala
@@ -3,15 +3,19 @@ import scala.annotation.tailrec
 @main
 def day5(): Unit = {
    val source = getSource("5_test.txt")
-   val rules: Seq[(Int, Int)] =
-      source.takeWhile(_ != "").map { line =>
-         val Array(first, last) = line.split("\\|").map(_.toInt)
-         (first, last)
-      }
+   val rules: Vector[(Int, Int)] =
+      source
+         .takeWhile(_ != "")
+         .map { line =>
+            val Array(first, last) = line.split("\\|").map(_.toInt)
+            (first, last)
+         }
+         .toVector
    val updates: Seq[Vector[Int]] =
       source.dropWhile(_ != "").tail.map(_.split(",").map(_.toInt).toVector)
 
    def getMiddleNumber(v: Vector[Int]): Int = v(v.length / 2)
+   def isValidPair(x: Int, y: Int): Boolean = rules.contains((x, y))
 
    @tailrec
    def isUpdateValid(v: Vector[Int], r: Seq[(Int, Int)]): Boolean =
@@ -23,15 +27,25 @@ def day5(): Unit = {
          else false
       }
 
-   @tailrec
-   def reOrderUpdate(v: Vector[Int], r: Seq[(Int, Int)]): Vector[Int] =
-      if (isUpdateValid(v, r)) v
-      else {
-        val rearranged: Vector[Int] = ???
-         reOrderUpdate(rearranged, r)
+   def rearrange(update: Vector[Int]): Vector[Int] = {
+      @tailrec
+      def checkPairs(v: Vector[Int]): Vector[Int] = {
+         val pairs = v.sliding(2).toVector
+         val (_, invalidPairs) = pairs.partition { case Vector(first, second) => isValidPair(first, second) }
+
+         if (invalidPairs.isEmpty) v
+         else {
+            val firstInvalidPair = invalidPairs.head
+            val swapped = v
+               .updated(v.indexOf(firstInvalidPair(0)), firstInvalidPair(1))
+               .updated(v.indexOf(firstInvalidPair(1)), firstInvalidPair(0))
+            checkPairs(swapped)
+         }
       }
+      checkPairs(update)
+   }
 
    println(s"1: ${updates.filter(isUpdateValid(_, rules)).map(getMiddleNumber).sum}")
-   println(s"2: ${updates.filterNot(isUpdateValid(_, rules)).map(v => reOrderUpdate(v, rules)).map(getMiddleNumber).sum}")
+   println(s"2: ${updates.filterNot(isUpdateValid(_, rules)).map(rearrange).map(getMiddleNumber).sum}")
 
 }
