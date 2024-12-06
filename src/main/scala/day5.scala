476c970d8d44dcb71248f6eca922dcd3af5c6193
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-06 10:00:29 +0100

    day5 2 missing implementation

--- src/main/scala/day5.scala
+++ src/main/scala/day5.scala
@@ -2,7 +2,7 @@ import scala.annotation.tailrec
 
 @main
 def day5(): Unit = {
-   val source = getSource("5.txt")
+   val source = getSource("5_test.txt")
    val rules: Seq[(Int, Int)] =
       source.takeWhile(_ != "").map { line =>
          val Array(first, last) = line.split("\\|").map(_.toInt)
@@ -14,16 +14,24 @@ def day5(): Unit = {
    def getMiddleNumber(v: Vector[Int]): Int = v(v.length / 2)
 
    @tailrec
-   def checkFwd(v: Vector[Int], r: Seq[(Int, Int)]): Boolean =
-      if (v.isEmpty) true
+   def isUpdateValid(v: Vector[Int], r: Seq[(Int, Int)]): Boolean =
+      if (v.length <= 1) true
       else {
          val head = v.head
          val tail = v.tail
-         if (tail.forall(i => r.contains((head, i)))) checkFwd(tail, r)
+         if (tail.forall(i => r.contains((head, i)))) isUpdateValid(tail, r)
          else false
       }
 
-   println(s"1: ${updates.map(u => if (checkFwd(u, rules)) getMiddleNumber(u) else 0).sum}")
-   // println(s"2: ${}")
+   @tailrec
+   def reOrderUpdate(v: Vector[Int], r: Seq[(Int, Int)]): Vector[Int] =
+      if (isUpdateValid(v, r)) v
+      else {
+        val rearranged: Vector[Int] = ???
+         reOrderUpdate(rearranged, r)
+      }
+
+   println(s"1: ${updates.filter(isUpdateValid(_, rules)).map(getMiddleNumber).sum}")
+   println(s"2: ${updates.filterNot(isUpdateValid(_, rules)).map(v => reOrderUpdate(v, rules)).map(getMiddleNumber).sum}")
 
 }
