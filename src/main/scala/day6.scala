4ff19587191ba6a0e9dfecb5b0de15dffa5ad2ed
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-06 14:15:39 +0100

    day6 minor refactoring

--- src/main/scala/day6.scala
+++ src/main/scala/day6.scala
@@ -7,16 +7,16 @@ def day6(): Unit = {
       case North, South, East, West
 
    val src = getSource("6.txt")
-   val start: String = src.filter(_.contains('^')).mkString
-   val linIndex = src.indexOf(start)
-   val posIndex = start.indexOf('^')
-   val startPos = (linIndex, posIndex)
-   val maxPos = src.head.indices.last
-   val maxLin = src.indices.last
-   val grid = src.updated(linIndex, src(linIndex).updated(startPos._2, '.'))
+   val turtle = src.find(_.contains('^')).get
+   val lin_Idx = src.indexOf(turtle)
+   val pos_Idx = turtle.indexOf('^')
+   val startPos = (lin_Idx, pos_Idx)
+   val max_X = src.head.indices.last
+   val max_Y = src.indices.last
+   val grid = src.updated(lin_Idx, src(lin_Idx).updated(pos_Idx, '.'))
 
    def isPosOutOfBounds(p: Position): Boolean =
-      p._1 > maxLin || p._1 < 0 || p._2 > maxPos || p._2 < 0
+      p._1 > max_Y || p._1 < 0 || p._2 > max_X || p._2 < 0
 
    def isNextStepClear(p: Position): Boolean =
       grid(p._1)(p._2) != '.'
@@ -29,12 +29,12 @@ def day6(): Unit = {
 
    @tailrec
    def walk(curDir: Direction, curPos: Position, visited: Set[Position]): Int = {
-      val (lin, pos) = curPos
+      val (y, x) = curPos
       val nextStep = curDir match
-         case Direction.North => (lin - 1, pos)
-         case Direction.South => (lin + 1, pos)
-         case Direction.East  => (lin, pos + 1)
-         case Direction.West  => (lin, pos - 1)
+         case Direction.North => (y - 1, x)
+         case Direction.South => (y + 1, x)
+         case Direction.East  => (y, x + 1)
+         case Direction.West  => (y, x - 1)
 
       if (isPosOutOfBounds(nextStep)) visited.size + 1
       else if (isNextStepClear(nextStep))
