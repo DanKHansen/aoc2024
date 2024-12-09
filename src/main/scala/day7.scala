5cb37176a31fc047db0e436dcf1c8495fc277f16
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-09 18:44:20 +0100

    day7 solution

--- src/main/scala/day7.scala
+++ src/main/scala/day7.scala
@@ -1,8 +1,19 @@
 @main
 def day7(): Unit = {
-   val src = getSource("7_test.txt")
+   val src = getSource("7.txt")
+   val testCases = src.map { case s"$result: $xs" => (result.toLong, xs.split(" ").map(_.toLong).toList) }
 
+   def check(value: Long, xs: List[Long], concatenation: Boolean = false): Boolean =
+      xs match
+         case ::(head, Nil) => head == value
+         case first :: second :: rest =>
+            check(value, (first * second) :: rest, concatenation) || check(
+              value,
+              (first + second) :: rest,
+              concatenation) ||
+            (concatenation && check(value, (first.toString ++ second.toString).toLong :: rest, concatenation))
+         case Nil => false
 
-   println(s"1: ${src}")
-   println(s"2: ${}")
+   println(s"1: ${testCases.map((testCase, xs) => if check(testCase, xs) then testCase else 0).sum}")
+   println(s"2: ${testCases.map((testCase, xs) => if check(testCase, xs, true) then testCase else 0).sum}")
 }
