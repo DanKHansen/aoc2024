978b92ecf8e2515fe8b6b41b9a0d7ec9ce90542d
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-09 20:09:21 +0100

    day8 playing around with structural types

--- src/main/scala/day8.scala
+++ src/main/scala/day8.scala
@@ -1,7 +1,18 @@
+@main
 def day8(): Unit = {
-   val src = getSource("8_test.txt")
+   val src = getSource("8.txt")
+   class Record(elems: (String, Any)*) extends Selectable:
+      private val fields = elems.toMap
+      def selectDynamic(name: String): Any = fields(name)
 
-   println(s"1: ${}")
-   println(s"2: ${}")
+   type Position = Record { val line: Int; val pos: Int }
+   type Antenna = Record { val freq: Char; val pos: Position }
+
+   val a: Antenna = Record("freq" -> 'A', "pos" -> Record("line" -> 4, "pos" -> 5)).asInstanceOf[Antenna]
+
+   println(s"Frequency: ${a.selectDynamic("freq")}, Position: (${a.pos.selectDynamic("pos")}, ${a.pos.selectDynamic("line")})")
+
+
+   // println(s"1: ${}")
+   // println(s"2: ${}")
 }
-
