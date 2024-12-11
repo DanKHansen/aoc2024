ae6079856390c3fad542b19c50a7fd9b8fa6a6c5
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-11 11:36:29 +0100

    day9 part 1 refactoring

--- src/main/scala/day9.scala
+++ src/main/scala/day9.scala
@@ -4,25 +4,30 @@ import scala.annotation.tailrec
 def day9(): Unit = {
    val src = getSource("9.txt")
    val list = src.mkString.grouped(2).zipWithIndex
-   val parsed = list
-      .map { case (fileSize, idx) =>
-         val size = fileSize.head.toString.toInt
-         val repeatCount = if (fileSize.length == 1) 0 else fileSize.last.toString.toInt
-         ((idx.toString + " ") * size).split(" ") ++ ("." * repeatCount)
-      }
-      .flatMap(_.toList)
-      .map(_.toString)
-      .toList
+   val parsed = list.flatMap { case (fileSize, idx) =>
+      val size = fileSize.head.asDigit
+      val dotRepeat = if (fileSize.length == 1) 0 else fileSize.last.asDigit
+      val idxString = (idx.toString + " ") * size
+      val dots = ". " * dotRepeat
+      (idxString + dots).split(" ")
+   }.toList
 
    def moveFilesFromBackToEmpty(l: List[String]): List[String] = {
       @tailrec
       def loop(in: List[String]): List[String] = {
-         val idxOfLastNumber = in.lastIndexOf(in.filterNot(_ == ".").last)
          val idxOfFirstDot = in.indexOf(".")
-         if idxOfFirstDot < idxOfLastNumber then {
-            val next = in.updated(idxOfFirstDot, in(idxOfLastNumber)).updated(idxOfLastNumber, ".")
-            loop(next)
-         } else in
+         val lastNonDot = in.filterNot(_ == ".").lastOption
+         lastNonDot match {
+            case Some(last) =>
+               val idxOfLastNumber = in.lastIndexOf(last)
+               if (idxOfFirstDot < idxOfLastNumber) {
+                  val next = in.updated(idxOfFirstDot, last).updated(idxOfLastNumber, ".")
+                  loop(next)
+               } else {
+                  in
+               }
+            case None => in
+         }
       }
       loop(l)
    }
@@ -31,6 +36,6 @@ def day9(): Unit = {
 
    def checkSum(l: List[(String, Int)]) = l.map(multiply).sum
 
-   println(s"1: ${checkSum(moveFilesFromBackToEmpty(parsed).filterNot(_ ==".").zipWithIndex)}")
+   println(s"1: ${checkSum(moveFilesFromBackToEmpty(parsed).filterNot(_ == ".").zipWithIndex)}")
    // println(s"2: ${}")
 }
