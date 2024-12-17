8821dbc5dc89cd1a1fd7502f9168affad45138ed
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-17 10:13:03 +0100

    day13 solution

--- src/main/scala/day13.scala
+++ src/main/scala/day13.scala
@@ -1,8 +1,42 @@
 @main
 def day13(): Unit = {
+   val src = getSource("13.txt").mkString("|")
+   case class Claw(ax: Long, ay: Long, bx: Long, by: Long, x: Long, y: Long):
+      def solve: Option[Long] = for
+         b <- (x * ay - y * ax) safeDiv (bx * ay - by * ax)
+         a <- (x - b * bx) safeDiv ax
+      yield a * 3 + b
 
-   val src = getSource("13_test.txt").toVector
+   object L:
+      def unapply(s: String): Option[Long] = s.toLongOption
 
-   println(s"1: ${}")
-   println(s"2: ${}")
+   object Claw:
+      def parse(xs: Seq[String]): Option[Claw] =
+         xs match
+            case Seq(
+                  s"Button A: X+${L(ax)}, Y+${L(ay)}",
+                  s"Button B: X+${L(bx)}, Y+${L(by)}",
+                  s"Prize: X=${L(x)}, Y=${L(y)}"
+                ) =>
+               Some(Claw(ax, ay, bx, by, x, y))
+            case _ => None
+
+   extension (a: Long)
+      infix def safeDiv(b: Long): Option[Long] =
+         Option.when(b != 0 && a % b == 0)(a / b)
+
+   def parse(input: String) = input.split("\\|+").toSeq.grouped(3).flatMap(Claw.parse).toSeq
+
+   def solve(c: Claw) =
+      for
+         a <- 0 to 100
+         b <- 0 to 100
+         if a * c.ax + b * c.bx == c.x
+         if a * c.ay + b * c.by == c.y
+      yield a * 3L + b
+
+   val diff = 10_000_000_000_000L
+
+   println(s"1: ${parse(src).flatMap(solve).sum}")
+   println(s"2: ${parse(src).map(c => c.copy(x = c.x + diff, y = c.y + diff)).flatMap(_.solve).sum}")
 }
