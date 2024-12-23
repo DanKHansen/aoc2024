5109ca502a548a21bbc8e72a2751b4ba5848871e
Dan Kjeldstrøm Hansen <dankh@live.dk>
2024-12-23 10:44:08 +0100

    day15 1 solution

--- src/main/scala/day15.scala
+++ src/main/scala/day15.scala
@@ -4,7 +4,7 @@ import sources.*
 import scala.annotation.tailrec
 object day15 {
    def main(args: Array[String]): Unit = {
-      val src = getSource("15_test2.txt")
+      val src = getSource("15.txt")
 
       case class Pos(lin: Int, col: Int)
 
@@ -44,7 +44,6 @@ object day15 {
             }
          } yield Pos(lin, col)
       }.head
-
       val wh = initialWH.updated(startPos.lin, initialWH(startPos.lin).updated(startPos.col, '.'))
 
       def isWall(p: Pos, wh: Vector[Vector[Char]] = wh): Boolean =
@@ -63,62 +62,50 @@ object day15 {
             case Direction.RIGHT => Pos(currPos.lin, currPos.col + 1)
             case Direction.LEFT  => Pos(currPos.lin, currPos.col - 1)
 
-      // For each movement: check if the next step is either free, a wall or a box.
-      //
-      // In case of a wall, do nothing,
-      // Start over with the next move
-      // In case of a '.' Update the WH by switching the position of the robot and the free space.
-      // Start over with the next move
-      // In case of a box: Build a list of boxes in front of the robot
-      //    check if the last box has a free space in front of it or not. (the previous 3 rules apply)
-      //    Each boxlist consists of n number of boxes, and a last element that is either
-      //    empty '.'
-      //    or a wall '#'
-      //    if last is a wall, do nothing
-      // Start over with next move
-      //    Otherwise 'move' the whole set of boxes one step forward,
-      //    Update the warehouse to its new state:
-      //    1. replace the last element with the first box,
-      //    2. replace the first box with the robot, and finally
-      //    3. replace the previous robot position with a '.'
-      // Start over with the next move
-      //
-      // calculate the resulting GPS's, and sum them up.
-
       @tailrec
       def go(currPos: Pos, steps: List[Direction], currWH: Vector[Vector[Char]]): Vector[Vector[Char]] =
-         if steps.isEmpty then currWH
-         else
-            moveFrom(currPos, steps.head) match {
-               case p if isWall(p) => go(currPos, steps.tail, currWH)
-               case p if isFree(p) => go(p, steps.tail, currWH)
-               case p if isBox(p) =>
-                  val boxes = makeBoxList(currPos, steps.head)
-                  val newWH = updateWH(boxes, steps.head, currWH)
-                  go(p, steps.tail, newWH)
+         if steps.isEmpty then {
+            // currWH.updated(currPos.lin, currWH(currPos.lin).updated(currPos.col, '@')) foreach println
+            currWH
+         } else
+            val next = moveFrom(currPos, steps.head)
+            next match {
+               case p if isWall(p, currWH) => go(currPos, steps.tail, currWH)
+               case p if isFree(p, currWH) => go(p, steps.tail, currWH)
+               case p if isBox(p, currWH) =>
+                  val boxes = makeBoxList(currPos, steps.head, currWH)
+                  if boxes.length > 1 then
+                     val newWH = updateWH(boxes, currWH)
+                     go(p, steps.tail, newWH)
+                  else go(currPos, steps.tail, currWH)
             }
 
-      def makeBoxList(headPos: Pos, dir: Direction): List[Pos] =
+      def makeBoxList(headPos: Pos, dir: Direction, wh: Vector[Vector[Char]]): List[Pos] =
          @tailrec
          def loop(pos: Pos, acc: List[Pos]): List[Pos] =
             val next = moveFrom(pos, dir)
             next match {
-               case p if isWall(p) => Nil
-               case p if isFree(p) => p :: Nil
-               case p if isBox(p)  => loop(next, pos :: acc)
+               case p if isWall(p, wh) => Nil
+               case p if isFree(p, wh) => p :: acc
+               case p if isBox(p, wh)  => loop(p, p :: acc)
             }
-         headPos :: loop(headPos, Nil)
+         headPos :: loop(headPos, Nil).reverse
 
-      def updateWH(boxes: List[Pos], dir: Direction, currWH: Vector[Vector[Char]]): Vector[Vector[Char]] =
-         val nwh = currWH.updated(boxes.tail.head.lin, currWH(boxes.tail.head.lin).updated(boxes.tail.head.col, '.'))
-         val nwh2 = nwh.updated(boxes.last.lin, nwh(boxes.last.lin).updated(boxes.last.col, 'O'))
-         nwh2
+      def updateWH(boxes: List[Pos], cWH: Vector[Vector[Char]]): Vector[Vector[Char]] =
+         val (box1, box2) = (boxes.tail.head, boxes.last)
+         val nwh = cWH.updated(box1.lin, cWH(box1.lin).updated(box1.col, '.'))
+         nwh.updated(box2.lin, nwh(box2.lin).updated(box2.col, 'O'))
 
-      //def calculateGPS: List[Int] = ???
+      def calculateGPS(wh: Vector[Vector[Char]]): Int = {
+         for
+            l <- wh.indices
+            c <- wh(l).indices
+         yield wh(l)(c) match
+            case 'O' => Some(Pos(l, c))
+            case _   => None
+      }.flatten.map(p => 100 * p.lin + p.col).sum
 
-      go(startPos, steps, wh)
-
-      // println(s"1: ${calculateGPS.sum}")
+      println(s"1: ${calculateGPS(go(startPos, steps, wh))}")
       // println(s"2: ${}")
    }
 }
