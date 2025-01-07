30fabe8e30d8e11e33ffe63c3f00bd700528cacf
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-01-07 11:41:20 +0100

    day15 2 work in progress

--- src/main/scala/day15.scala
+++ src/main/scala/day15.scala
@@ -3,15 +3,15 @@ import sources.*
 import scala.annotation.tailrec
 object day15:
    def main(args: Array[String]): Unit =
-      val src = getSource("15.txt")
+      val src = getSource("15_2_smalltest.txt")
 
       case class Pos(lin: Int, col: Int)
 
       enum Direction:
-         case UP, DOWN, RIGHT, LEFT
+         case UP, DOWN, RIGHT, LEFT, NE, SE, SW, NW
 
       enum TileType:
-         case WALL, FREE, BOX, ROBOT, LEFT, RIGHT
+         case WALL, FREE, BOX, ROBOT, LEFT_SIDE, RIGHT_SIDE
 
       object TileType:
          def apply(c: Char): TileType = c match
@@ -19,15 +19,27 @@ object day15:
             case 'O' => BOX
             case '.' => FREE
             case '@' => ROBOT
-            case '[' => LEFT
-            case ']' => RIGHT
+            case '[' => LEFT_SIDE
+            case ']' => RIGHT_SIDE
+
+         def unapply(t: TileType): Option[Char] = t match
+            case WALL       => Some('#')
+            case BOX        => Some('O')
+            case FREE       => Some('.')
+            case ROBOT      => Some('@')
+            case LEFT_SIDE  => Some('[')
+            case RIGHT_SIDE => Some(']')
 
       object Direction:
          val offsets: Map[Direction, Pos] = Map(
            UP -> Pos(-1, 0),
            DOWN -> Pos(1, 0),
            RIGHT -> Pos(0, 1),
-           LEFT -> Pos(0, -1)
+           LEFT -> Pos(0, -1),
+           NE -> Pos(-1, 1),
+           SE -> Pos(1, 1),
+           SW -> Pos(1, -1),
+           NW -> Pos(-1, -1)
          )
          def apply(c: Char): Direction = c match
             case '^' => UP
@@ -35,12 +47,27 @@ object day15:
             case '>' => RIGHT
             case '<' => LEFT
 
-      def parse(in: List[String]) =
-         val warehouse = in.takeWhile(_.nonEmpty)
-         val attempts = in.drop(warehouse.size + 1)
-         (warehouse.map(_.toVector).toVector, attempts.mkString.toList.map(Direction(_)))
+      def parse1(in: List[String]) =
+         val warehouse = in.takeWhile(_.nonEmpty).map(_.toVector).toVector
+         val attempts = in.drop(warehouse.size + 1).mkString.toList.map(Direction(_))
+         (warehouse, attempts)
 
-      val (initialWH, steps) = parse(src)
+      def parse2(in: List[String]) =
+         val warehouse = in
+            .takeWhile(_.nonEmpty)
+            .map(s =>
+               s.flatMap {
+                  case '#' => Seq('#', '#')
+                  case '.' => Seq('.', '.')
+                  case 'O' => Seq('[', ']')
+                  case '@' => Seq('@', '.')
+               }.toVector)
+            .toVector
+         val attempts = in.drop(warehouse.size + 1).mkString.toList.map(Direction(_))
+         (warehouse, attempts)
+
+      val (initialWH, steps) = parse2(src)
+
       val startPos = initialWH.zipWithIndex.collectFirst {
          case (col, lin) if col.contains('@') => Pos(lin, col.indexOf('@'))
       }.get
@@ -55,15 +82,17 @@ object day15:
 
       @tailrec
       def go(currPos: Pos, steps: List[Direction], currWH: Vector[Vector[Char]]): Vector[Vector[Char]] =
+         currWH foreach println
          steps match
             case Nil => currWH
             case dir :: tail =>
                val next = moveFrom(currPos, dir)
                val boxes = makeBoxList(currPos, dir, currWH)
                (tileType(next, currWH), boxes.length > 1) match
-                  case (TileType.FREE, _)   => go(next, tail, currWH)
-                  case (TileType.BOX, true) => go(next, tail, updateWH(boxes, currWH))
-                  case _                    => go(currPos, tail, currWH)
+                  case (TileType.FREE, _) => go(next, tail, currWH)
+                  case (TileType.BOX, _) | (TileType.LEFT_SIDE, _) | (TileType.RIGHT_SIDE, _) =>
+                     go(next, tail, updateWH(boxes, currWH))
+                  case _ => go(currPos, tail, currWH)
 
       def makeBoxList(headPos: Pos, dir: Direction, wh: Vector[Vector[Char]]): List[Pos] =
          @tailrec
@@ -71,14 +100,21 @@ object day15:
             val next = moveFrom(pos, dir)
             tileType(next, wh) match
                case TileType.FREE => next :: acc
-               case TileType.BOX  => loop(next, next :: acc)
-               case _             => Nil
+               case TileType.BOX | TileType.LEFT_SIDE | TileType.RIGHT_SIDE =>
+                  loop(next, next :: acc)
+               case _ => Nil
          headPos :: loop(headPos, Nil).reverse
 
       def updateWH(boxes: List[Pos], cWH: Vector[Vector[Char]]): Vector[Vector[Char]] =
-         val (box1, box2) = (boxes.tail.head, boxes.last)
-         val nwh = cWH.updated(box1.lin, cWH(box1.lin).updated(box1.col, '.'))
-         nwh.updated(box2.lin, nwh(box2.lin).updated(box2.col, 'O'))
+         //println(s"Boxes: $boxes")
+         //println(tileType(boxes.tail.head, wh) == TileType.RIGHT_SIDE)
+         tileType(boxes.tail.head, wh) match
+            case TileType.BOX =>
+               val (box1, box2) = (boxes.tail.head, boxes.last)
+               val nwh = cWH.updated(box1.lin, cWH(box1.lin).updated(box1.col, '.'))
+               nwh.updated(box2.lin, nwh(box2.lin).updated(box2.col, 'O'))
+            case TileType.RIGHT_SIDE | TileType.LEFT_SIDE => cWH
+            case _ => cWH
 
       def calculateGPS(wh: Vector[Vector[Char]]): Int =
          wh.indices
@@ -86,5 +122,7 @@ object day15:
             .map(p => 100 * p.lin + p.col)
             .sum
 
+      go(startPos, steps, wh)
+
       println(s"1: ${calculateGPS(go(startPos, steps, wh))}")
-      // println(s"2: ${}")
+      // println(s"2: ${parse2(src)}")
