3072b17a7115831be60c5105f7bec4df6b6d6642
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-01-10 10:53:12 +0100

    update

--- src/main/scala/day15b.scala
+++ src/main/scala/day15b.scala
@@ -1,6 +1,8 @@
 import sources.*
-case class Pos(lin: Int, col: Int)
+
 object day15b:
+   case class Pos(lin: Int, col: Int)
+
    def main(args: Array[String]): Unit =
       val (wh, steps) = getSource("15_2_smalltest.txt").span(_.nonEmpty)
       val wideWH = wh.map(_.map(c => if c == 'O' then "[]" else s"$c$c").mkString)
