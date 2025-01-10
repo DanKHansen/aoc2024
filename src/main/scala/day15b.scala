02dc673ed636333f37ea07695dd556ba94572811
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-01-10 10:51:15 +0100

    update

--- src/main/scala/day15b.scala
+++ src/main/scala/day15b.scala
@@ -1,14 +1,15 @@
 import sources.*
-class day15b
 case class Pos(lin: Int, col: Int)
 object day15b:
    def main(args: Array[String]): Unit =
       val (wh, steps) = getSource("15_2_smalltest.txt").span(_.nonEmpty)
       val wideWH = wh.map(_.map(c => if c == 'O' then "[]" else s"$c$c").mkString)
-      val initPos = wideWH.zipWithIndex.collectFirst {
-         case (col, lin) if col.contains('@') => Pos(lin, col.indexWhere(_ == '@'))
-      }.getOrElse(Pos(0,0))
-      val initWH = wideWH.updated(initPos.lin, wideWH(initPos.lin).replace("@@",".."))
+      val initPos = wideWH.zipWithIndex
+         .collectFirst {
+            case (col, lin) if col.contains('@') => Pos(lin, col.indexWhere(_ == '@'))
+         }
+         .getOrElse(Pos(0, 0))
+      val initWH = wideWH.updated(initPos.lin, wideWH(initPos.lin).replace("@@", ".."))
 
       initWH foreach println
 
